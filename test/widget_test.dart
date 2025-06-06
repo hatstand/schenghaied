866df1cbb347582a813c453d43702d3b820e7187
John Maguire <john.maguire@gmail.com>
2025-06-06 17:02:04 +0100

    dart fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:schengen/models/stay_record.dart';
 import 'package:time_machine/time_machine.dart';
